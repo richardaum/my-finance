f917eb551cb7520108b151d9c9f393dc4eaf4f3d
Richard Lopes <richard.lopes92@gmail.com>
2024-02-13 15:34:25 -0300

    refactor: Rename

--- src/components/EntryTable/EntryTable.component.tsx
+++ src/components/EntryTable/EntryTable.component.tsx
@@ -3,16 +3,7 @@
 import { type IconProp } from "@fortawesome/fontawesome-svg-core";
 import { faCalendarTimes, faCircleCheck, faWarning, faRepeat } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  Checkbox,
-  Table as MantineTable,
-  ScrollArea,
-  ThemeIcon,
-  rem,
-  type DefaultMantineColor,
-  Group,
-  Text,
-} from "@mantine/core";
+import { Checkbox, Table, ScrollArea, ThemeIcon, rem, type DefaultMantineColor, Group, Text } from "@mantine/core";
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useGetEntriesQuery } from "~/hooks/useGetEntriesQuery";
@@ -47,34 +38,34 @@ export function EntryTable(props: Props) {
 
   return (
     <ScrollArea>
-      <MantineTable miw={800} verticalSpacing="sm">
-        <MantineTable.Thead>
-          <MantineTable.Tr>
-            <MantineTable.Th style={{ width: rem(40) }}>
+      <Table miw={800} verticalSpacing="sm">
+        <Table.Thead>
+          <Table.Tr>
+            <Table.Th style={{ width: rem(40) }}>
               <Checkbox
                 onChange={toggleAll}
                 checked={selection.length === entries.length}
                 indeterminate={selection.length > 0 && selection.length !== entries.length}
               />
-            </MantineTable.Th>
-            <MantineTable.Th>{t("entryTable.heading.status")}</MantineTable.Th>
-            <MantineTable.Th>{t("entryTable.heading.date")}</MantineTable.Th>
-            <MantineTable.Th>{t("entryTable.heading.description")}</MantineTable.Th>
-            <MantineTable.Th>{t("entryTable.heading.category")}</MantineTable.Th>
-            <MantineTable.Th>{t("entryTable.heading.account")}</MantineTable.Th>
-            <MantineTable.Th ta="right">{t("entryTable.heading.amount")}</MantineTable.Th>
-            <MantineTable.Th ta="right">{t("entryTable.heading.actions")}</MantineTable.Th>
-          </MantineTable.Tr>
-        </MantineTable.Thead>
-        <MantineTable.Tbody>
+            </Table.Th>
+            <Table.Th>{t("entryTable.heading.status")}</Table.Th>
+            <Table.Th>{t("entryTable.heading.date")}</Table.Th>
+            <Table.Th>{t("entryTable.heading.description")}</Table.Th>
+            <Table.Th>{t("entryTable.heading.category")}</Table.Th>
+            <Table.Th>{t("entryTable.heading.account")}</Table.Th>
+            <Table.Th ta="right">{t("entryTable.heading.amount")}</Table.Th>
+            <Table.Th ta="right">{t("entryTable.heading.actions")}</Table.Th>
+          </Table.Tr>
+        </Table.Thead>
+        <Table.Tbody>
           {entries.map((entry) => {
             const selected = selection.includes(entry.id);
             return (
-              <MantineTable.Tr key={entry.id} bg={selected ? "var(--mantine-color-blue-light)" : undefined}>
-                <MantineTable.Td>
+              <Table.Tr key={entry.id} bg={selected ? "var(--mantine-color-blue-light)" : undefined}>
+                <Table.Td>
                   <Checkbox checked={selection.includes(entry.id)} onChange={() => toggleRow(entry.id)} />
-                </MantineTable.Td>
-                <MantineTable.Td>
+                </Table.Td>
+                <Table.Td>
                   {entry.status === "PENDING" && (
                     <Icon color="yellow" icon={faWarning} title={t(`entryTable.status.${entry.status}`)} />
                   )}
@@ -84,23 +75,23 @@ export function EntryTable(props: Props) {
                   {entry.status === "PAID" && (
                     <Icon color="green" icon={faCircleCheck} title={t(`entryTable.status.${entry.status}`)} />
                   )}
-                </MantineTable.Td>
-                <MantineTable.Td>{date.format(new Date(entry.date))}</MantineTable.Td>
-                <MantineTable.Td>
+                </Table.Td>
+                <Table.Td>{date.format(new Date(entry.date))}</Table.Td>
+                <Table.Td>
                   <Group gap="xs">
                     <Text size="sm">{entry.description}</Text>
                     {entry.repeatId && <Icon color="blue" icon={faRepeat} title={t("entryTable.repeat")} />}
                   </Group>
-                </MantineTable.Td>
-                <MantineTable.Td>{entry.category.name}</MantineTable.Td>
-                <MantineTable.Td>{entry.account.name}</MantineTable.Td>
-                <MantineTable.Td ta="right">{currency.format(entry.amount)}</MantineTable.Td>
-                <MantineTable.Td />
-              </MantineTable.Tr>
+                </Table.Td>
+                <Table.Td>{entry.category.name}</Table.Td>
+                <Table.Td>{entry.account.name}</Table.Td>
+                <Table.Td ta="right">{currency.format(entry.amount)}</Table.Td>
+                <Table.Td />
+              </Table.Tr>
             );
           })}
-        </MantineTable.Tbody>
-      </MantineTable>
+        </Table.Tbody>
+      </Table>
     </ScrollArea>
   );
 }
